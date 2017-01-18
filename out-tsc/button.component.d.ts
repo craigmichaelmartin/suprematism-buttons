5b06f9d8257ea74bb7a9b0abf9513c1411081470
Craig Martin <craigmichaelmartin@gmail.com>
2017-01-18 12:14:14 -0500

    style additions per new mocks

--- out-tsc/button.component.d.ts
+++ out-tsc/button.component.d.ts
@@ -3,4 +3,9 @@ export declare class ButtonComponent {
     color: Color;
     text: string;
     icon: string;
+    disabled: boolean;
+    corners: boolean;
+    height: number;
+    defaultHeight: string;
+    swallowIfDisabled(event: any): void;
 }
--- src/button.component.ts
+++ src/button.component.ts
@@ -8,7 +8,19 @@ export type Color = 'green' | 'blue';
   styles: [require('./button.component.css')]
 })
 export class ButtonComponent {
+
   @Input() color: Color = 'green';
   @Input() text: string;
   @Input() icon: string;
+  @Input() disabled: boolean;
+  @Input() corners: boolean;
+  @Input() height: number;
+  defaultHeight = '36px';
+
+  swallowIfDisabled(event) {
+    if (this.disabled) {
+      event.stopPropagation();
+    }
+  }
+
 }
